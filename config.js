f6d48532272eaecb600779c5d2b0c1c66d2aec9b
garenafreefireqr-blip <garenafreefireqr@gmail.com>
2026-06-05 14:22:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,16 @@
+
 const APP_CONFIG = {
-
   SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzvgfN2UJ8v1sTFXmBMgFCxf8NzEOu9-VLueR9TjcbOwkg7Ghb2wYz4zHhAbw6GMV7D8g/exec",
-
   UPI_ID: "9014844036@superyes",
-
   EVENT_NAME: "Wedding of ram & Sita",
-
   ORG_NAME: "Event Organisation",
-
   CURRENCY: "INR",
-
   MIN_AMOUNT: 50,
-
   MAX_AMOUNT: 0,
-
   THEME_COLOR: "#6366f1",
-
-  VERSION: "2.0"
-
+  VERSION: "3"
 };
-
 window.APP_CONFIG = APP_CONFIG;
+ENDOFFILE
+
+
